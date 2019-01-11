f2f4475142e07b9e0210091244f77a6458f24555
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-11 01:44:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   if num > 3
     #test to see if prime
   
-  elsif num > 0 #1-3 are prime
+  elsif num > 1 # 2&3 are prime
     return true
   
   else #negitive numbers are not prime
