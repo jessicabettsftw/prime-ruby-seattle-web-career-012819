50e44234e46d9359aa0861d6c671f88b14fa581c
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-11 02:17:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
 def prime?(num)
   
   if (num > 3)
-    mid = num // 2
-    puts "mid is #{mid}"
+    mid = num / 2
     #array of numbers to test
     test_numbers = []
     start = 2
@@ -10,7 +9,7 @@ def prime?(num)
       test_numbers.push(start)
       start += 1
     end
-    
+    test_numbers.inspect
     #loop through test numbers array to see if prime
     test_numbers.each do |test_num|
       if (num % test_num) == 0
